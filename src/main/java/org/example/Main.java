8fdbacc7a1df0b83f7fb0bd1752701236eb1074d
108 <108@academicos.cesde.edu.co>
2023-12-04 15:34:58 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreUsuario = "samuel bermudez gaviria";
+        short edadUsuario = 17;
+        String telefonoUsuario = "3052273445";
+        String dcumentoIdentidad = "1017927144";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJUlio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario = "CLL 50# 80-20";
+        double costoInscribcion = 150000.0;
+        float tasaineteresNatillera = 0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valos especofico a una variable
+        //desde el teclado (recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("hola soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+
+
+    }
+}
